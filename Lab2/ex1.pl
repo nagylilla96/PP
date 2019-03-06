9193441df929b85c84510ab9514041d022aaa8cb
nagylilla96 <n.lilla96@gmail.com>
2019-03-06 11:12:30 +0200

    Finished ex1, started ex2

--- Lab2/ex1.pl
+++ Lab2/ex1.pl
@@ -1,3 +1,5 @@
+% ex1
+
 is_prime(2).
 is_prime(X) :-
     X > 1,
@@ -8,3 +10,28 @@ prime(X, Y) :-
     0 is X mod Y -> false;
     Z is Y + 1,
     prime(X, Z).
+
+list_primes(A, B, []) :- A > B, !.
+
+list_primes(A, B, [A|L]) :-
+    is_prime(A),
+    !,
+    A1 is A + 1,
+    list_primes(A1, B, L).
+
+list_primes(A, B, L) :-
+    A1 is A + 1,
+    list_primes(A1, B, L).
+
+list_primes(A, B, L) :-
+    A =< 2,
+    A =< B,
+    append([], [2], L).
+
+
+all_primes(A, B, L, T) :-
+    list_primes(A, B, L),
+    statistics(runtime, [T|_]).
+
+% ex2
+
