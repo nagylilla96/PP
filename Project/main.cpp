0bc2a79186950f3da277c4610a0d15dc7112769f
nagylilla96 <n.lilla96@gmail.com>
2019-04-17 11:45:33 +0300

    Added gmp solution *cries*

--- Project/main.cpp
+++ Project/main.cpp
@@ -2,22 +2,34 @@
 #include <vector>
 #include <cmath>
 #include <limits.h>
-#include <boost/multiprecision/cpp_int.hpp>
-#include <boost/lexical_cast.hpp>
+#include <gmp.h>
+#include <string.h>
+#include <chrono>
 using namespace std;
-using namespace boost::multiprecision;
+using namespace std::chrono;
 
 #define START 2
 #define END ULLONG_MAX
 
-int is_prime(uint1024_t x)
+int is_prime(const mpz_t x)
 {
-	for (uint1024_t i = 2; i <= (uint1024_t) boost::multiprecision::sqrt(x); i++)
+	mpz_t xx;
+	mpz_init_set(xx, x);	
+	mpz_t square, i;
+	mpz_init(square);
+	mpz_sqrt(square, xx);
+	mpz_init_set_si(i, 2);
+	
+	while (mpz_cmp(i, square) <= 0)
 	{
-		if (x % i == 0)
+		mpz_t res;
+		mpz_init(res);
+		mpz_fdiv_r(res, x, i);
+		if (mpz_cmp_si(res, 0) == 0)
 		{
 			return 0;
 		}
+		mpz_add_ui(i, i, 1);
 	}
 	return 1;
 }
@@ -26,106 +38,98 @@ int is_prime(uint1024_t x)
 vector<int> getPrimes()
 {
 	vector<int> primes;
-	int i = 2;
+	mpz_t i;
+	mpz_init_set_si(i, 3);
 
 	while (primes.size() < 100)
 	{
 		if (is_prime(i))
 		{
-			primes.push_back(i);
+			primes.push_back(mpz_get_ui(i));
 		}
-		i++;
+		mpz_add_ui(i, i, 1);
 	}
 	return primes;
 }
 
-void print_results(vector<uint1024_t> nums, int n)
+void print_results(vector<int> nums, int n)
 {
 	for (auto const& x : nums)
 	{
-		cout << (uint1024_t) x << " ";
+		cout <<  x << " ";
 	}
 }
 
-uint1024_t concat(uint1024_t a, uint1024_t b)
-{
-	uint1024_t c = b;
-	do
-	{
-		a *= 10;
-		c /= 10;
-	} while (c);
+//void concatenate(mpz_t ret, mpz_t a, mpz_t b)
+//{
+//	mpz_t c, res;
+//	mpz_init_set(c, b);
+//	do
+//	{
+//		mpz_mul(a, a, 10);
+//		mpz_fdiv_q(c, c, 10);
+//	} while (c);
+//	
+//	mpz_init_set(res, a);
+//	mpz_add(res, res, b);
+//}
 
-	return a + b;
-}
-
-void smarandache(uint1024_t a, uint1024_t b)
+void smarandache(mpz_t a, mpz_t b)
 {
 	vector<int> primes = getPrimes();
-	uint1024_t limit = (uint1024_t(1) << 1024) - 1;
 	int index = 0;
-	uint1024_t conc = primes.at(index);
+	mpz_t conc;
+	mpz_init_set_si(conc, 2);
+	
+	gmp_printf ("a = %Zd \n", a);
+	gmp_printf ("b = %Zd \n", b);
+	
+	//cout << "EEEELO" << endl;
 
-	if (b < 2) return;
+	if (mpz_cmp_ui(conc, 2) > 0) return;
+	
+	//cout << "EEEEEELO" << endl;
 	
 	for (auto const& x: primes)
 	{
-		cout << "WHAT" << endl;
-		if (conc > b)
+		//cout << "WHAT" << endl;
+		if (mpz_cmp(conc, b) > 0)
 		{
-			cout << "{" << conc.str() << "}";
+			//cout << "{" << conc.str() << "}";
 			return;
 		}
-		if (is_prime(conc) && (conc >= a))
+		if (is_prime(conc) && (mpz_cmp(conc, a) >= 0))
 		{
-			cout << conc.str() << " ";
+			cout << ".";
+			gmp_printf ("%Zd \n", conc);
 		}
-		conc = boost::lexical_cast<uint1024_t>(conc.str() + to_string(primes.at(++index)));
-		cout << "[" << conc.str() << "]";
-		
+		char *mystr = (char*) malloc(1000);
+		mpz_get_str(mystr, 10, conc);
+		string str(mystr);
+		strcpy(mystr, (str + to_string(x)).c_str());
+		mpz_set_str(conc, mystr, 10);
+		gmp_printf ("\n[%Zd]\n ", conc);	
 	}
-}
+} 	
 
-int main(int argc, char** argv)
+int main()
 {
-	uint1024_t a;
-	uint1024_t b;
-	uint1024_t limit = (uint1024_t(1) << 1024) - 1;
-	
-	cout << "Limit: " << limit.str() << endl;
+	mpz_t a;
+	mpz_init_set_si(a, 2);
+	mpz_t b;
+	mpz_init_set_str(b, "2357111317192329313741434753596167717379838997101104", 10);
+	//uint1024_t b = 100000;
 
-	if (argc != 3)
-	{
-		if (argc == 1)
-		{
-			a = START;
-			b = END;
-		}
-		else
-		{
-			cout << "Usage: main.exe <start> <end>" << endl;
-			return -1;
-		}
-	}
+	//print_results(getPrimes(), 100);
 
-	string::size_type sz = 0;
-	a = boost::lexical_cast<uint1024_t>(argv[1]);
-	b = boost::lexical_cast<uint1024_t>(argv[2]);
 	vector<int> primes;
-	vector<uint1024_t> smars;
-
-	if (a > b)
-	{
-		uint1024_t c = b;
-		b = a;
-		a = c;
-	}
-	
-	b = (uint1024_t(1) << 1024) - 1;
 
 	try
 	{
+		auto start = chrono::high_resolution_clock::now();
 		smarandache(a, b);
+		auto finish = std::chrono::high_resolution_clock::now();
+		cout << "Time = " << duration_cast<chrono::duration<double>>(finish - start).count() << " s" << endl;
 	}
 	catch (exception &e)
 	{
