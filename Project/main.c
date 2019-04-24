c5c053d116003609a01f4187f6c859eb8232d727
nagylilla96 <n.lilla96@gmail.com>
2019-04-24 10:33:51 +0300

    Finished c++ version, added c version (without threads)

--- Project/main.c
+++ Project/main.c
@@ -1,15 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 #include <limits.h>
 #include <gmp.h>
 #include <string.h>
-#include <chrono>
-using namespace std;
-using namespace std::chrono;
-
-#define START 2
-#define END ULLONG_MAX
+#include <time.h>
 
 int is_prime(const mpz_t x)
 {
@@ -32,42 +27,47 @@ int is_prime(const mpz_t x)
 }
 
 // Get the first 100 primes
-vector<int> getPrimes()
+unsigned long int* getPrimes(int *n)
 {
-	vector<int> primes;
+	unsigned long int *primes = (unsigned long int*) calloc(100, sizeof(unsigned long int));
 	mpz_t i;
 	mpz_init_set_si(i, 3);
-
-	while (primes.size() < 100)
+	int size = 0;
+	
+	while (size < 100)
 	{
 		if (is_prime(i))
 		{
-			primes.push_back(mpz_get_ui(i));
+			primes[size] = mpz_get_ui(i);
+			size++;
 		}
 		mpz_add_ui(i, i, 1);
 	}
+	*n = size;
 	mpz_clear(i);
+	
 	return primes;
 }
 
-void print_results(vector<int> nums, int n)
+void print_results(unsigned long int* nums, int n)
 {
-	for (auto const& x : nums)
+	for (int i = 0; i < n; i++)
 	{
-		cout <<  x << " ";
+		printf("%lu ", nums[i]);
 	}
 }
 
 void smarandache(mpz_t a, mpz_t b)
 {
-	vector<int> primes = getPrimes();
+	int length = 0;
+	unsigned long int* primes = getPrimes(&length);
 	int index = 0;
 	mpz_t conc;
 	mpz_init_set_si(conc, 2);
 
 	if (mpz_cmp_ui(conc, 2) > 0) return;
 	
-	for (auto const& x: primes)
+	for (int i = 0; i < length; i++)
 	{
 		if (mpz_cmp(conc, b) > 0)
 		{
@@ -76,14 +76,15 @@ void smarandache(mpz_t a, mpz_t b)
 		}
 		if (is_prime(conc) && (mpz_cmp(conc, a) >= 0))
 		{
-			gmp_printf ("%Zd \n", conc);
+			gmp_printf("%Zd \n", conc);
 		}
 		char *mystr = (char*) malloc(1000);
+		char *temp = (char*) malloc(1000);
 		mpz_get_str(mystr, 10, conc);
-		string str(mystr);
-		strcpy(mystr, (str + to_string(x)).c_str());
+		snprintf(temp, 1000, "%s%lu", mystr, primes[i]);
+		strcpy(mystr, temp);
 		mpz_set_str(conc, mystr, 10);
-		gmp_printf ("\n[%Zd]\n ", conc);
+		//gmp_printf("\n[%Zd]\n ", conc);
 	}
 	mpz_clear(conc);
 } 	
@@ -92,21 +93,17 @@ int main()
 {
 	mpz_t a;
 	mpz_t b;
+	clock_t start, end;
+	double cpu_time_used;
 	
 	mpz_init_set_si(a, 2);
-	mpz_init_set_str(b, "2357111317192329313741434753596167717379838997101102", 10);
+	mpz_init_set_str(b, "2357111317192329313741434753596167717379838997101103", 10);
 
-	try
-	{
-		auto start = chrono::high_resolution_clock::now();
-		smarandache(a, b);
-		auto finish = std::chrono::high_resolution_clock::now();
-		cout << "Time = " << duration_cast<chrono::duration<double>>(finish - start).count() << " s" << endl;
-	}
-	catch (exception &e)
-	{
-		cout << e.what() << endl;
-	}
+	start = clock();
+	smarandache(a, b);
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("Time = %lf s", cpu_time_used);
 	
 	mpz_clear(a);
 	mpz_clear(b);
