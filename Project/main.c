6c5431b923c34decdb7a5234f2c43d23d32f3bae
nagylilla96 <n.lilla96@gmail.com>
2019-04-29 21:02:38 +0300

    Added multithreading for c

--- Project/main.c
+++ Project/main.c
@@ -12,6 +12,7 @@ mpz_t b;
 int threads;
 unsigned long long int* primes;
 int length;
+mpz_t *numbers;
 
 int is_prime(const mpz_t x)
 {
@@ -35,15 +36,15 @@ int is_prime(const mpz_t x)
 	return 1;
 }
 
-// Get the first 100 primes
+// Get the first primes
 unsigned long long int* getPrimes(int *n)
 {
-	unsigned long long int *primes = (unsigned long long int*) calloc(2000, sizeof(unsigned long long int));
+	unsigned long long int *primes = (unsigned long long int*) calloc(1750, sizeof(unsigned long long int));
 	mpz_t i;
 	mpz_init_set_si(i, 3);
 	int size = 0;
 	
-	while (size < 2000)
+	while (size < 1750)
 	{
 		if (is_prime(i))
 		{
@@ -66,12 +67,12 @@ void print_results(unsigned long long int* nums, int n)
 	}
 }
 
-void smarandache(unsigned long long int *primes, int id)
+void getNumbers()
 {
+	numbers = malloc(sizeof(mpz_t) * length);
+	
 	mpz_t conc;
 	mpz_init_set_si(conc, 2);
-	
-	//printf("\nid = %d, threads = %d\n", id, threads); 
 
 	if (mpz_cmp_ui(conc, 2) > 0) 
 	{
@@ -79,38 +80,51 @@ void smarandache(unsigned long long int *primes, int id)
 		return;
 	}
 	
-	for (int i = id; i < length; i += threads)
+	for (int i = 0; i < length; i++)
 	{
 		if (mpz_cmp(conc, b) > 0)
 		{
 			mpz_clear(conc);
 			return;
 		}
-		if (mpz_probab_prime_p(conc, 15) == 2 && (mpz_cmp(conc, a) >= 0))
-		{
-			gmp_printf("%Zd \n", conc);
-		}
+		mpz_init_set(numbers[i], conc);
 		char *mystr = (char*) malloc(10000);
 		char *temp = (char*) malloc(10000);
 		mpz_get_str(mystr, 10, conc);
 		snprintf(temp, 10000, "%s%llu", mystr, primes[i]);
 		strcpy(mystr, temp);
 		mpz_set_str(conc, mystr, 10);		
-		//gmp_printf ("[%Zd] \n", conc);
 		free(mystr);
 		free(temp);
 	}
 	mpz_clear(conc);
+}
+
+void smarandache(unsigned long long int *primes, int id)
+{	
+	printf("\nid = %d, threads = %d\n", id, threads); 
+	
+	for (int i = id; i < length; i += threads)
+	{
+		if (mpz_probab_prime_p(numbers[i], 15) == 2 && (mpz_cmp(numbers[i], a) >= 0))
+		{
+			gmp_printf("%Zd \n", numbers[i]);
+		}
+	}
 } 	
 
 void *threadFunc(void *args)
 {
-	int* id = (int*) args;
+	int id = *((int*) args);
 	
-	smarandache(primes, *id);
+	smarandache(primes, id);
+	
+	printf("\nFinished running %d\n", id);	
+	
+	return NULL;
 }
 
-void run(int threads)
+void run()
 {
 	clock_t start, end;
 	double cpu_time_used;
@@ -120,30 +134,39 @@ void run(int threads)
 	for (int i = 0; i < threads; i++) 
 	{
 		pthread_create(&th[i], NULL, threadFunc, &i);
+		printf("Creating thread %d\n", i);
 	}	
-	pthread_exit(NULL);
+	for (int i = 0; i < threads; i++)
+	{
+		pthread_join(th[i], NULL);
+	}
 	end = clock();
 	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 	printf("Time = %lf s", cpu_time_used);
 }
 
-int main()
+int main(int argc, char **argv)
 {
 	
 	mpz_init_set_si(a, 2);
 	mpz_init(b);
 	mpz_ui_pow_ui(b, 2, 1000000);
 	primes = getPrimes(&length);
+	getNumbers();
+	
+	if (argc != 2) return -1;
+	threads = atoi(argv[1]);
+	run();
 
-	for (int i = 1; i <= 8; i++) 
-	{
-		printf("\nRunning with %d threads!\n", i);
-		threads = i;
-		run(threads);
-	}
+	//for (int i = 1; i <= 8; i++) 
+	//{
+	//	printf("\nRunning with %d threads!\n", i);
+	//	threads = i;
+	//	run(threads);
+	//}
 	
 	mpz_clear(a);
 	mpz_clear(b);
-
+	free(numbers);
 	return 0;
 }
