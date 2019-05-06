41000aec6913c1bef3e6fbf7b6ca15bcf7ae5bec
nagylilla96 <n.lilla96@gmail.com>
2019-05-06 21:22:46 +0300

    I wanna commit this before it's too late

--- Project/main.pl
+++ Project/main.pl
@@ -28,34 +28,63 @@ list_primes(A, B, L) :-
     A =< B,
     append([], [2], L).
 	
-smarandache(_, _, _, []).
-smarandache(_, B, Num, _) :-
+list_numbers(_, _, _, [], _).
+
+list_numbers(_, B, Num, _, []) :-
 	Num > B.
 	
-smarandache(A, B, Num, [F|E]) :-
+list_numbers(A, B, Num, [F|E], L) :-
 	A > Num,
-	smarandache(A, B, F, E).	
+	list_numbers(A, B, F, E, L).		
 	
-smarandache(A, B, Num, [F|E]) :-
-	not(crypto_is_prime(Num, [])),
+list_numbers(A, B, Num, [F|E], [Num|L]) :-
 	atom_concat(Num, F, X),
 	atom_number(X, Y),
-	%write('['),
-	%write(Num),
-	%writeln(']'),
-	smarandache(A, B, Y, E).
+	%writeln(Y),
+	list_numbers(A, B, Y, E, L).
+	
+smarandache([]).
 
-smarandache(A, B, Num, [F|E]) :-
-	crypto_is_prime(Num, []),
-	writeln(Num),
-	atom_concat(Num, F, X),
-	atom_number(X, Y),
-	smarandache(A, B, Y, E).
+smarandache([F|E]) :-
+	not(crypto_is_prime(F, [])),
+	smarandache(E).
+
+smarandache([F|E]) :-
+	crypto_is_prime(F, []),
+	writeln(F),
+	smarandache(E).
 	
-ex(T) :- 
+worker(L
+	
+create_threads(0, _).
+	
+create_threads(Threads, L) :-
+	Id is Threads - 1,
+	thread_create(worker(L), Id, []),
+	create_threads(Id, L).
+	
+create_workers(Id, List, Threads) :-
+	forall(between(1, Threads, _),
+	thread_create(do_work(Id, List), _, [])).
+	
+join_threads(0).
+	
+join_threads(Threads) :-
+	Id is Threads - 1,
+	thread_join(Id, S),
+	join_threads(Id).
+	
+master(Threads, L) :-
+	create_threads(Threads, L),
+	join_threads(Threads).
+
+	
+ex(Time, Threads) :- 
 	A is 2,
-	B is 2**1000000,
-	statistics(walltime, _),
+	B is 2**100000,	
 	list_primes(2, 7500, [F|L]), 
-	smarandache(A, B, F, L),
-	statistics(walltime, [_|[T]]).
+	list_numbers(A, B, F, L, C),
+	statistics(walltime, _),
+	master(Threads, C),
+	smarandache(C),
+	statistics(walltime, [_|[Time]]).
